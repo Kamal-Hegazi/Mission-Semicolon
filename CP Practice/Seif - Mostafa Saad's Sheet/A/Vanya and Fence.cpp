e077fc5a4acaf436e4c790a74e9c64657e2c12ab
Vseif1011 <seifm6295@gmail.com>
2025-08-18 15:23:46 +0300

    day1

---
+++ CP Practice/Seif - Mostafa Saad's Sheet/A/Vanya and Fence.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int a=0;
+    int d=0;
+    for (int i=0;i<n;i++) {
+        if (s[i]=='A') {
+            a++;
+        } else if (s[i]=='D') {
+            d++;
+        }
+    }
+    if (a==d) {
+        cout<<"Friendship";
+    } else if (a>d) {
+        cout<<"Anton";
+    } else {
+        cout<<"Danik";
+    }
+
+     return 0;
+}
